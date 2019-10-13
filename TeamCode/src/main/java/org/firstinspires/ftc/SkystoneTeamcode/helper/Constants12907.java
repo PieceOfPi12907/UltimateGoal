c540e19d57e7ed324bb7a42322a925dc9bea58b7
Shreya Ragade <sragade3@gmail.com>
2019-10-13 16:20:11 -0700

    fixing the constants (merge issues)

--- TeamCode/src/main/java/org/firstinspires/ftc/SkystoneTeamcode/helper/Constants12907.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/SkystoneTeamcode/helper/Constants12907.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.SkystoneTeamcode.helper;
 
 public class Constants12907 {
     public static enum Direction{
-        FWD, BWD, LEFT, RIGHT, TURN;
+        STRAIGHT, LEFT, RIGHT, TURN;
     }
 
 }
