1a660b7d016c385d3a1422cd9c16fb8748371ed8
Your Name <you@example.com>
2019-12-08 12:47:55 -0800

    LEAGUE 3 WIP CODE: parameters program w/ updated webcam & hashmap

--- TeamCode/src/main/java/org/firstinspires/ftc/SkystoneTeamcode/helper/Constants12907.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/SkystoneTeamcode/helper/Constants12907.java
@@ -13,5 +13,26 @@ public class Constants12907 {
     public static enum DumperArmDropPositions{
         DROP1,DROP2,DROP3,DOWN;
     }
+
+    public static String BACK_LEFT_MOTOR = "backLeft";
+    public static String FRONT_LEFT_MOTOR = "frontLeft";
+    public static String BACK_RIGHT_MOTOR = "backRight";
+    public static String FRONT_RIGHT_MOTOR = "frontRight";
+
+    public static String PIVOT_GRABBER_SERVO = "pivotGrabber";
+    public static String BLOCK_CLAMPER_SERVO = "blockClamper";
+    public static String RIGHT_REPOSITIONING_SERVO = "rightRepositioning";
+    public static String LEFT_REPOSITIONING_SERVO = "leftRepositioning";
+
+    public static String BLUE_FLAG = "blueFlag";
+    public static String OUTER_FLAG = "outerFlag";
+    public static String TELEMETRY = "telemetry";
+
+    public static String SKY_POSITION = "skystonePosition";
+
+    public static String IMU = "imu";
+    public static String QUARRY_DISTANCE_SENSOR = "quarryDistance";
+    public static String FRONT_COLOR_SENSOR = "frontColor";
+    public static String BACK_COLOR_SENSOR = "backColor";
 }
 
