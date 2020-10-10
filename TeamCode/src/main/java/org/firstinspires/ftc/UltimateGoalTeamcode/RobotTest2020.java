914155b98c7bbaf17cdda2f98796d55f9505f833
MenakaW <MeniRoshi@hotmail.com>
2020-10-10 08:38:06 -0700

    Added RobotTest class inside of our new TeamCode folder.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/UltimateGoalTeamcode/RobotTest2020.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.UltimateGoalTeamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class RobotTest2020 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
