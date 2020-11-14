083769a86326d96783922e40cfbc3f7b101dc907
MenakaW <MeniRoshi@hotmail.com>
2020-11-14 12:10:09 -0800

    11/14/2020 Changes: - renamed auto, focused on motor, got rid of attachment

--- TeamCode/src/main/java/org/firstinspires/ftc/UltimateGoalTeamcode/tester/SimultaneousTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/UltimateGoalTeamcode/tester/SimultaneousTester.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "simulataneous", group = "auto")
+@Autonomous(name = "simultaneous", group = "auto")
 public class SimultaneousTester extends LinearOpMode {
 
     DcMotor motor;
@@ -17,22 +17,25 @@ public class SimultaneousTester extends LinearOpMode {
         waitForStart();
         if(opModeIsActive()){
             motor.setPower(0.5);
-            //motor.setTargetPosition(5000);
-            /*while(motor.getCurrentPosition()<5000){
+            motor.setTargetPosition(5000);
+            while(motor.getCurrentPosition()<5000){
                 motor.setPower(0.5);
-                if(attachment.getPosition()>0.52|| attachment.getPosition()<0.48){
+                /*if(attachment.getPosition()>0.52|| attachment.getPosition()<0.48){
                     attachment.setPosition(0.5);
                 }
+                 */
+                sleep(100);
             }
-            */
         }
     }
 
     public void initialize(){
         motor = hardwareMap.get(DcMotor.class, "motor");
-        attachment = hardwareMap.get(Servo.class, "servo");
+        /*attachment = hardwareMap.get(Servo.class, "servo");
         attachment.setPosition(0.2);
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+         */
     }
 }
